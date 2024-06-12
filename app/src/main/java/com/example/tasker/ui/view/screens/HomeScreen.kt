72d7d7eb9725dee2bec75f829a7f61e034d2ff23
Marvin Ramos <mr487223@gmail.com>
2024-06-12 15:26:51 -0600

    refact(remove): remove import n useless code

--- app/src/main/java/com/example/tasker/ui/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tasker.ui.view.components.AddButtonComponent
@@ -19,7 +18,6 @@ import com.example.tasker.ui.view.components.ToolBarComponent
 @Preview
 @Composable
 fun HomeScreen() {
-    val context = LocalContext.current
     Scaffold(
         topBar = { ToolBarComponent() },
         floatingActionButton = { AddButtonComponent()},
