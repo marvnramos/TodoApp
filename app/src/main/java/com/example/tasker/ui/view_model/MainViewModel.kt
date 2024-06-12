977c5cc612be521493ce06bce95296daf2699248
Marvin Ramos <mr487223@gmail.com>
2024-06-12 13:33:56 -0600

    feat(MainViewModel): add mainviewmodel file

---
+++ app/src/main/java/com/example/tasker/ui/view_model/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.tasker.ui.view_model
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJqd3QtYXVkaWVuY2UiLCJpc3MiOiJ5b3VyLWRvbWFpbi5jb20iLCJ1c2VySWQiOiJjN2VmM2YzYS1mYjYxLTQxMzEtYmI0My1mNjk0YTZlZWU0NzAiLCJleHAiOjE3MTU5NzE2ODB9.ZgOybuoeFh0y2Gp75lB3LQHq9l_6y0zyY8Gz3uCn4FM"
+
+}
