48c709e3d12ff83d27848002b40cb257e381027b
Marvin Ramos <mr487223@gmail.com>
2024-06-12 15:25:00 -0600

    feat(AddButton): add floating action button

---
+++ app/src/main/java/com/example/tasker/ui/view/components/AddButtonComponent.kt
@@ -0,0 +1,27 @@
+package com.example.tasker.ui.view.components
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.core.content.ContextCompat
+import com.example.tasker.R
+
+
+@Preview
+@Composable
+fun AddButtonComponent(){
+    val context = LocalContext.current
+    FloatingActionButton(
+        onClick = { /*TODO: Open 2nd screen*/ },
+        containerColor = Color(ContextCompat.getColor(context, R.color.orange)),
+        contentColor = Color(ContextCompat.getColor(context, R.color.white)),
+
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "Floating action button.")
+    }
+}
