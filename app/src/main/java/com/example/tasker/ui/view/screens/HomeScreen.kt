542e744d63043811dfb56257ee1505dd3f1537e0
Marvin Ramos <mr487223@gmail.com>
2024-06-12 15:25:36 -0600

    feat(HomeScreen): add components to screen

---
+++ app/src/main/java/com/example/tasker/ui/view/screens/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.example.tasker.ui.view.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tasker.ui.view.components.AddButtonComponent
+import com.example.tasker.ui.view.components.CardComponent
+import com.example.tasker.ui.view.components.ToolBarComponent
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+    Scaffold(
+        topBar = { ToolBarComponent() },
+        floatingActionButton = { AddButtonComponent()},
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 56.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            CardComponent()
+        }
+
+    }
+}
