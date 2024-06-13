bff46f396f331929f1dbdd7e0106543bb55d9c50
Marvin Ramos <mr487223@gmail.com>
2024-06-13 09:16:26 -0600

    feat(statusIcon): add statusIcon property

--- app/src/main/java/com/example/tasker/ui/view/theme/CardProperties.kt
+++ app/src/main/java/com/example/tasker/ui/view/theme/CardProperties.kt
@@ -4,5 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 class CardProperties (
     var statusBackgroundColor: Color,
-    var statusIconColor: Color
+    var statusIconColor: Color,
+    var statusIcon: Int
 )
