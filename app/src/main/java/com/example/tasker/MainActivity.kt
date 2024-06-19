010b9e74333491cf3d29027a83f56ab4e07da70a
Marvin Ramos <mr487223@gmail.com>
2024-06-19 10:43:17 -0600

    refactor(color): remove bg color

--- app/src/main/java/com/example/tasker/MainActivity.kt
+++ app/src/main/java/com/example/tasker/MainActivity.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.tasker.data.models.Get
 import com.example.tasker.ui.view.screens.HomeScreen
 import com.example.tasker.ui.view.theme.TaskerTheme
@@ -50,7 +51,6 @@ class MainActivity : ComponentActivity() {
             TaskerTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     HomeScreen()
                 }
