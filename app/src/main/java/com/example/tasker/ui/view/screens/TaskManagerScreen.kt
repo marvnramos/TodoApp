1ee28cbfb3e7a4710a8d73a210726f7d7e6f4167
Marvin Ramos <mr487223@gmail.com>
2024-06-19 10:46:26 -0600

    refactor(taskmanager): add components

--- app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
@@ -1,42 +1,65 @@
 package com.example.tasker.ui.view.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.example.tasker.R
 import com.example.tasker.ui.view.components.AddButtonComponent
 import com.example.tasker.ui.view.components.TaskManagerBarComponent
 import com.example.tasker.ui.view.components.TextFieldComponent
 
-
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
-fun TaskManagerScreen(){
-    val context = LocalContext.current
+fun TaskManagerScreen() {
+    val robotoFamily = FontFamily(
+        Font(R.font.roboto_regular, FontWeight.Normal),
+        Font(R.font.roboto_light, FontWeight.Light),
+        Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.roboto_medium, FontWeight.Medium),
+        Font(R.font.roboto_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+        Font(R.font.roboto_bold, FontWeight.Bold),
+        Font(R.font.roboto_bolditalic, FontWeight.Bold, FontStyle.Italic)
+    )
     Scaffold(
         topBar = { },
         floatingActionButton = { AddButtonComponent() },
-    ){
+    ) {
         TaskManagerBarComponent()
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 75.dp)
-//                .verticalScroll(rememberScrollState())
-                .background(Color(ContextCompat.getColor(context, R.color.white))),
-
-            ) {
+        ) {
+            Text(
+                text = "Título",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+//                fontFamily = robotoFamily,
+//                fontWeight = FontWeight.Normal
+            )
+            TextFieldComponent(true)
+            Text(
+                text = "Descripción",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+//                fontFamily = robotoFamily,
+//                fontWeight = FontWeight.Normal
+            )
             TextFieldComponent(false)
         }
     }
