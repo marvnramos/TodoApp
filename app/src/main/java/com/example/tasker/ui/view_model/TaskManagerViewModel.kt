a154f87ec3b6f5ab6fad6d0a9f50784f7a1a2e59
Marvin Ramos <mr487223@gmail.com>
2024-06-21 14:57:34 -0600

    refactor(TaskManagerViewModel): add attributes and fun to manage live data from task manager screen "inputs"

---
+++ app/src/main/java/com/example/tasker/ui/view_model/TaskManagerViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.tasker.ui.view_model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TaskManagerViewModel: ViewModel() {
+
+    private val _title = MutableLiveData<String>()
+    val title: LiveData<String> = _title
+
+
+    private val _description = MutableLiveData<String>()
+    val description: LiveData<String> = _description
+
+    fun onTextFieldChanged(title: String, description: String){
+        _title.value = title
+        _description.value = description
+    }
+}
