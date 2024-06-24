7469a8c4a8a6233aad15394d04766530c394c4ee
Marvin Ramos <mr487223@gmail.com>
2024-06-24 15:33:24 -0600

    feat(status component): add status components

--- app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
@@ -1,19 +1,28 @@
 package com.example.tasker.ui.view.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tasker.data.models.Status
 import com.example.tasker.ui.view.components.taskmanager_components.DescriptionFieldComponent
+import com.example.tasker.ui.view.components.taskmanager_components.StatusComponent
 import com.example.tasker.ui.view.components.taskmanager_components.TaskManagerBarComponent
 import com.example.tasker.ui.view.components.taskmanager_components.TitleFieldComponent
 import com.example.tasker.ui.view_model.TaskManagerViewModel
@@ -24,6 +33,8 @@ import com.example.tasker.ui.view_model.TaskManagerViewModel
 fun TaskManagerScreen( viewModel: TaskManagerViewModel = TaskManagerViewModel()) {
     val title: String by viewModel.title.observeAsState(initial = "")
     val description: String by viewModel.description.observeAsState(initial = "")
+    val selectedStatus = remember { mutableStateOf<Status?>(null) }
+
 //    val robotoFamily = FontFamily(
 //        Font(R.font.roboto_regular, FontWeight.Normal),
 //        Font(R.font.roboto_light, FontWeight.Light),
@@ -36,7 +47,7 @@ fun TaskManagerScreen( viewModel: TaskManagerViewModel = TaskManagerViewModel())
     Scaffold(
         topBar = { },
     ) {
-        TaskManagerBarComponent(title, description)
+        TaskManagerBarComponent(title, description, selectedStatus)
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -70,6 +81,38 @@ fun TaskManagerScreen( viewModel: TaskManagerViewModel = TaskManagerViewModel())
 
             DescriptionFieldComponent(text = description){
                 viewModel.onTextFieldChanged(title, it)
+            }
+
+            Text(
+                text = "Estado",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+//                fontFamily = robotoFamily,
+//                fontWeight = FontWeight.Normal
+            )
+            Row (
+                modifier = Modifier
+                    .horizontalScroll(rememberScrollState())
+                    .padding(10.dp)
+            ){
+                StatusComponent(
+                    status = Status.IN_PROGRESS,
+                    isSelected = selectedStatus.value == Status.IN_PROGRESS,
+                    onStatusSelected = { selectedStatus.value = Status.IN_PROGRESS }
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                StatusComponent(
+                    status = Status.DONE,
+                    isSelected = selectedStatus.value == Status.DONE,
+                    onStatusSelected = { selectedStatus.value = Status.DONE }
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                StatusComponent(
+                    status = Status.TODO,
+                    isSelected = selectedStatus.value == Status.TODO,
+                    onStatusSelected = { selectedStatus.value = Status.TODO }
+                )
             }
         }
     }
