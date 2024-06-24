b1eb2df60c2474fee6a7f364a32adb319d3a61a2
Marvin Ramos <mr487223@gmail.com>
2024-06-24 15:36:05 -0600

    refactor(onStatusSelected): add status tracker

--- app/src/main/java/com/example/tasker/ui/view/components/taskmanager_components/StatusComponent.kt
+++ app/src/main/java/com/example/tasker/ui/view/components/taskmanager_components/StatusComponent.kt
@@ -18,17 +18,13 @@ import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.tasker.R
@@ -36,14 +32,16 @@ import com.example.tasker.data.models.Status
 import com.example.tasker.data.models.StatusProperties
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun StatusComponent(status: Status = Status.IN_PROGRESS){
+fun StatusComponent(
+    status: Status,
+    isSelected: Boolean,
+    onStatusSelected: () -> Unit
+) {
     val context = LocalContext.current
-    var selected by remember { mutableStateOf(false) }
-    val statusProperties = remember(status){
-        when(status){
-            Status.DONE-> StatusProperties(
+    val statusProperties = remember(status) {
+        when (status) {
+            Status.DONE -> StatusProperties(
                 statusText = "Hecho",
                 statusColor = Color(ContextCompat.getColor(context, R.color.green)),
                 statusIcon = R.drawable.done_icon
@@ -62,17 +60,16 @@ fun StatusComponent(status: Status = Status.IN_PROGRESS){
     }
 
     FilterChip(
-        selected = selected ,
-        onClick = { selected = !selected },
+        selected = isSelected,
+        onClick = onStatusSelected,
         label = { Text(text = statusProperties.statusText) },
         modifier = Modifier.then(
-            if(selected){
+            if (isSelected) {
                 Modifier.border(
                     BorderStroke(2.dp, Color(ContextCompat.getColor(context, R.color.darkBlue))),
                     shape = RoundedCornerShape(8.dp)
-                )
-                    .padding(0.dp)
-            }else{
+                ).padding(0.dp)
+            } else {
                 Modifier.padding(0.dp)
             }
         ),
@@ -81,39 +78,38 @@ fun StatusComponent(status: Status = Status.IN_PROGRESS){
                 colors = CardDefaults.cardColors(containerColor = statusProperties.statusColor),
                 modifier = Modifier
                     .padding(top = 9.dp, bottom = 9.dp, start = 0.dp)
-                    .size(50.dp, 50.dp),
-            )
-            {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    .size(35.dp, 35.dp),
+            ) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                     Image(
-                        modifier = Modifier.size(25.dp,25.dp),
+                        modifier = Modifier.size(25.dp, 25.dp),
                         painter = painterResource(id = statusProperties.statusIcon),
                         contentDescription = stringResource(id = R.string.dog_content_description)
                     )
                 }
             }
         },
-        trailingIcon = if (selected){
+        trailingIcon = if (isSelected) {
             {
                 Box(
                     Modifier.border(
                         BorderStroke(2.dp, Color.Black),
                         shape = RoundedCornerShape(1.dp),
                     )
-                ){
+                ) {
                     Icon(
                         Icons.Rounded.Check,
-                        contentDescription =  "",
+                        contentDescription = "",
                         tint = Color(ContextCompat.getColor(context, R.color.blue))
                     )
                 }
             }
-        }else{
+        } else {
             null
         },
         colors = FilterChipDefaults.filterChipColors(
             containerColor = Color(ContextCompat.getColor(context, R.color.white)),
             selectedContainerColor = Color(ContextCompat.getColor(context, R.color.white))
         )
-        )
-    }
+    )
+}
