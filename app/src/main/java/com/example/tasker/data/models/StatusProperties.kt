b5997130bfa6f41721e60fef66c02e12a60c0dbc
Marvin Ramos <mr487223@gmail.com>
2024-06-24 11:03:28 -0600

    feat(statusproperties): add status class

---
+++ app/src/main/java/com/example/tasker/data/models/StatusProperties.kt
@@ -0,0 +1,9 @@
+package com.example.tasker.data.models
+
+import androidx.compose.ui.graphics.Color
+
+class StatusProperties (
+    var statusText: String,
+    var statusColor: Color,
+    var statusIcon: Int
+)
