2abfde1756d8ef4a35802966d5a1f10a2eacb6d9
Marvin Ramos <mr487223@gmail.com>
2024-06-28 11:15:52 -0600

    refactor(FilterChip): add imports for new changes

--- app/src/main/java/com/example/tasker/ui/view/components/taskmanager_components/StatusComponent.kt
+++ app/src/main/java/com/example/tasker/ui/view/components/taskmanager_components/StatusComponent.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
@@ -25,7 +26,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import com.example.tasker.R
 import com.example.tasker.data.models.Status
