ef47bd723dec00d1086c93832be948b588139f5f
Marvin Ramos <mr487223@gmail.com>
2024-06-28 11:13:30 -0600

    refactor(TaskManagerScreen): add Icons section

--- app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/TaskManagerScreen.kt
@@ -2,11 +2,13 @@ package com.example.tasker.ui.view.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -17,11 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.tasker.R
 import com.example.tasker.data.models.Status
 import com.example.tasker.ui.view.components.taskmanager_components.DescriptionFieldComponent
+import com.example.tasker.ui.view.components.taskmanager_components.IconComponent
 import com.example.tasker.ui.view.components.taskmanager_components.StatusComponent
 import com.example.tasker.ui.view.components.taskmanager_components.TaskManagerBarComponent
 import com.example.tasker.ui.view.components.taskmanager_components.TitleFieldComponent
@@ -113,6 +118,29 @@ fun TaskManagerScreen( viewModel: TaskManagerViewModel = TaskManagerViewModel())
                     isSelected = selectedStatus.value == Status.TODO,
                     onStatusSelected = { selectedStatus.value = Status.TODO }
                 )
+            }
+            Text(
+                text = "Icono",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp),
+//                fontFamily = robotoFamily,
+//                fontWeight = FontWeight.Normal
+            )
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(60.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                IconComponent(R.drawable.book)
+                IconComponent(R.drawable.coffe)
+                IconComponent(R.drawable.clock)
+                IconComponent(R.drawable.pc)
+                IconComponent(R.drawable.thinking)
+                IconComponent(R.drawable.workout)
             }
         }
     }
