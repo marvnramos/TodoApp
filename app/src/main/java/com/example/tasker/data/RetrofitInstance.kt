a840046e6896d6fcc7e0085e29c15559ae1c087f
marvnramos <mframos@innovacion.gob.sv>
2024-09-25 08:49:03 -0600

    feat(buildconfig): add builconfig to get base url api

--- app/src/main/java/com/example/tasker/data/RetrofitInstance.kt
+++ app/src/main/java/com/example/tasker/data/RetrofitInstance.kt
@@ -1,10 +1,12 @@
+package com.example.tasker.data
+
 import com.example.tasker.data.repository.ApiRepository
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.example.tasker.BuildConfig
 
 object RetrofitInstance {
-//    private const val BASE_URL = "http://127.0.0.1:8080/"
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = BuildConfig.API_BASE_URL
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
