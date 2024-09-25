094ac4f3eee7ffd72486c75d388381399f6dc5c7
marvnramos <mframos@innovacion.gob.sv>
2024-09-25 09:35:51 -0600

    feat(colors): add pallet color

--- app/src/main/java/com/example/tasker/ui/view/theme/Color.kt
+++ app/src/main/java/com/example/tasker/ui/view/theme/Color.kt
@@ -2,10 +2,33 @@ package com.example.tasker.ui.view.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Definición de los colores personalizados
+val White = Color(0xFFF8FAFC)
+val LightYellow = Color(0xFFF5D565)
+val LightPink = Color(0xFFF5E8D5)
+val Orange = Color(0xFFE9A23B)
+val OrangeSoft = Color(0xFFFFEBB2)
+val Mint = Color(0xFFA0ECB1)
+val Green = Color(0xFF32D657)
+val Pink = Color(0xFFF7D4D3)
+val Red = Color(0xFFDD524C)
+val LightGrey = Color(0xFFE3E8EF)
+val Grey = Color(0xFF97A3B6)
+val DarkGrey = Color(0xFF868E96)
+val DarkBlue = Color(0xFF3662E3)
+val Blue = Color(0xFF1971C2)
+val LightBlue = Color(0xFFA5D8FF)
+val RedLight = Color(0xFFFB6D48)
+val Cream = Color(0xFFD49D10)
+val GreyTitle = Color(0xFFE9ECEF)
+val GreyTitleBorder = Color(0xFF868E96)
+val GreyDate = Color(0xFFCED4DA)
+
+// Definición de colores existentes
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
