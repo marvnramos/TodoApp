fdd6923692b943cd8c2bbab839e1db11f066d6af
marvnramos <mframos@innovacion.gob.sv>
2024-09-26 15:31:49 -0600

    feat(dependency): add google font dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.7.2")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
