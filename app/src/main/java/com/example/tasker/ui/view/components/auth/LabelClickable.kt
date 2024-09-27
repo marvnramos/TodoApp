890c312b245fc94987ea663fd6309567aa381a68
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 10:33:18 -0600

    feat(ui): add label clickable component

---
+++ app/src/main/java/com/example/tasker/ui/view/components/auth/LabelClickable.kt
@@ -0,0 +1,30 @@
+package com.example.tasker.ui.view.components.auth
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LabelClickable(
+    question: String,
+    action: String,
+    onClick: () -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = question,
+            style = MaterialTheme.typography.bodySmall
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        ClickableTextComponent(
+            text = action,
+            onClick = { onClick() }
+        )
+    }
+}
