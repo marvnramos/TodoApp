cd4c9a0920e5163aadbe4c42be1ef7a49466a984
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 10:35:09 -0600

    feat(ui): add submit button component

---
+++ app/src/main/java/com/example/tasker/ui/view/components/auth/SubmitButton.kt
@@ -0,0 +1,47 @@
+package com.example.tasker.ui.view.components.auth
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SubmitButton(
+    text: String, onClick: () -> Unit, enabled: Boolean
+) {
+    val colorScheme = MaterialTheme.colorScheme
+    OutlinedButton(
+        onClick = {
+
+        },
+        enabled = enabled,
+        modifier = Modifier
+            .width(250.dp)
+            .height(68.dp)
+            .padding(10.dp),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.Black),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorScheme.primary
+        )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Iniciar sesión", style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
