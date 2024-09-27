9ab48212e2fb087828603b561d4ae6aee20815f1
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 10:30:56 -0600

    feat(ui): add clickable text component

---
+++ app/src/main/java/com/example/tasker/ui/view/components/auth/ClickableText.kt
@@ -0,0 +1,25 @@
+package com.example.tasker.ui.view.components.auth
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun ClickableTextComponent(
+    text: String,
+    onClick: () -> Unit
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodySmall.copy(
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.SemiBold
+        ),
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    )
+}
