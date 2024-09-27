8b89ce173546ba5d2e78962caae17941a03f8009
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 15:03:05 -0600

    feat(ui): add sing up screen

--- app/src/main/java/com/example/tasker/ui/view/screens/SingUpScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/SingUpScreen.kt
@@ -1,96 +1,45 @@
 package com.example.tasker.ui.view.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.lifecycle.MutableLiveData
 import com.example.tasker.ui.view.components.auth.EmailTextField
-import com.example.tasker.ui.view.components.auth.LabelClickable
-import com.example.tasker.ui.view.components.auth.SubmitButton
-import com.example.tasker.ui.view.components.auth.WelcomeText
+import com.example.tasker.ui.view.components.auth.SingUpTemplate
 import com.example.tasker.ui.view.theme.TaskerTheme
 import com.example.tasker.ui.view_model.AuthViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen(authVM: AuthViewModel = AuthViewModel()) {
     val email by authVM.email.observeAsState(initial = "")
-    val isValidEmail = if (email.isNotEmpty()) authVM.isEmailValid(MutableLiveData(email)) else true
+    val isValidEmail =
+        if (email.isNotEmpty()) authVM.isEmailValid(MutableLiveData(email)) else true
 
     val errorMessage = if (!isValidEmail) "Dirección de correo inválida" else null
 
     val isError = errorMessage != null
     val isAvailable = email.isNotEmpty() && !isError
-
-    val colorScheme = MaterialTheme.colorScheme
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {},
-                navigationIcon = {
-                    IconButton(onClick = { /* Acción de retroceso */ }) {
-                        Icon(
-                            imageVector = Icons.Rounded.ArrowBack,
-                            contentDescription = "Menú Icono",
-                            tint = colorScheme.onSurface
-                        )
-                    }
-                }
-            )
-        },
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(horizontal = 35.dp, vertical = 20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            WelcomeText("Registra una nueva cuenta")
-            Spacer(modifier = Modifier.height(25.dp))
-            EmailTextField(
-                text = email,
-                isValid = isValidEmail,
-                errorMessage = errorMessage,
-                onTextChanged = { authVM.onValueChanged(email = it) }
-            )
-
-            Spacer(modifier = Modifier.height(25.dp))
-            SubmitButton(text = "Siguiente", onClick = { /*TODO*/ }, enabled = isAvailable)
-
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(bottom = 20.dp),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LabelClickable(
-                    question = "¿Ya tienes una cuenta?",
-                    action = "Inicia sesión",
-                    onClick = {}
-                )
-            }
-        }
+    val context = LocalContext.current
+    SingUpTemplate(
+        onArrowClick = { Toast.makeText(context, "Arrow Clicked", Toast.LENGTH_SHORT).show() },
+        onTextClick = { Toast.makeText(context, "Text Clicked", Toast.LENGTH_SHORT).show() },
+        onSubmitClick = { Toast.makeText(context, "Submit Clicked", Toast.LENGTH_SHORT).show() },
+        subWelcomeText = "Registra una nueva cuenta",
+        textIndicator = null,
+        submitText = "Siguiente",
+        isAvailable = isAvailable,
+    ) {
+        EmailTextField(
+            text = email,
+            isValid = isValidEmail,
+            errorMessage = errorMessage,
+            onTextChanged = { authVM.onValueChanged(email = it) }
+        )
     }
 }
 
