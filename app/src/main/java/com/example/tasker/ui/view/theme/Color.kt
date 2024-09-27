c07a13f53d2b552acb5371c80e7213322d6c595f
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 10:31:55 -0600

    feat(ui): ordering colors

--- app/src/main/java/com/example/tasker/ui/view/theme/Color.kt
+++ app/src/main/java/com/example/tasker/ui/view/theme/Color.kt
@@ -2,27 +2,42 @@ package com.example.tasker.ui.view.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Definición de los colores personalizados
-val White = Color(0xFFF8FAFC)
-val LightYellow = Color(0xFFF5D565)
-val LightPink = Color(0xFFF5E8D5)
+/**
+ * Definición de los colores personalizados
+ */
+
+// Colores Primarios
 val Orange = Color(0xFFE9A23B)
-val OrangeSoft = Color(0xFFFFEBB2)
-val Mint = Color(0xFFA0ECB1)
+val Blue = Color(0xFF1971C2)
 val Green = Color(0xFF32D657)
+
+// Colores Secundarios
+val LightYellow = Color(0xFFF5D565)
+val Mint = Color(0xFFA0ECB1)
 val Pink = Color(0xFFF7D4D3)
-val Red = Color(0xFFDD524C)
+val LightPink = Color(0xFFF5E8D5)
+val OrangeSoft = Color(0xFFFFEBB2)
+val LightBlue = Color(0xFFA5D8FF)
+
+// Colores de Fondo y Superficie
+val White = Color(0xFFF8FAFC)
+val Cream = Color(0xFFD49D10)
 val LightGrey = Color(0xFFE3E8EF)
+val GreyTitle = Color(0xFFE9ECEF)
+val GreyDate = Color(0xFFCED4DA)
+
+// Colores de Texto y Bordes
 val Grey = Color(0xFF97A3B6)
 val DarkGrey = Color(0xFF868E96)
-val DarkBlue = Color(0xFF3662E3)
-val Blue = Color(0xFF1971C2)
-val LightBlue = Color(0xFFA5D8FF)
-val RedLight = Color(0xFFFB6D48)
-val Cream = Color(0xFFD49D10)
-val GreyTitle = Color(0xFFE9ECEF)
 val GreyTitleBorder = Color(0xFF868E96)
-val GreyDate = Color(0xFFCED4DA)
+
+// Colores de Error
+val Red = Color(0xFFDD524C)
+val RedLight = Color(0xFFFB6D48)
+
+// Otros Colores
+val DarkBlue = Color(0xFF3662E3)
+
 
 // Definición de colores existentes
 val Purple80 = Color(0xFFD0BCFF)
