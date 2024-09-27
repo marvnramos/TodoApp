5045324acc70593087597d06dc067db41c65c773
marvnramos <mframos@innovacion.gob.sv>
2024-09-27 13:54:16 -0600

    feat(ui): add welcome text component

--- app/src/main/java/com/example/tasker/ui/view/screens/AuthScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/AuthScreen.kt
@@ -1,31 +1,25 @@
 package com.example.tasker.ui.view.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.MutableLiveData
-import com.example.tasker.R
 import com.example.tasker.ui.view.components.auth.LabelClickable
 import com.example.tasker.ui.view.components.auth.PasswordTextField
 import com.example.tasker.ui.view.components.auth.SubmitButton
 import com.example.tasker.ui.view.components.auth.UsernameTextField
+import com.example.tasker.ui.view.components.auth.WelcomeText
 import com.example.tasker.ui.view.theme.TaskerTheme
 import com.example.tasker.ui.view_model.AuthViewModel
 
@@ -34,7 +28,8 @@ fun AuthScreen(authVM: AuthViewModel = AuthViewModel()) {
     val username by authVM.username.observeAsState(initial = "")
     val password by authVM.password.observeAsState(initial = "")
 
-    val isValidUsername= if (username.isNotEmpty()) authVM.isEmailValid(MutableLiveData(username)) else true
+    val isValidUsername =
+        if (username.isNotEmpty()) authVM.isEmailValid(MutableLiveData(username)) else true
     val isAvailable = password.isNotEmpty() && isValidUsername && username.isNotEmpty()
 
 
@@ -45,34 +40,7 @@ fun AuthScreen(authVM: AuthViewModel = AuthViewModel()) {
 
         ) {
         Spacer(modifier = Modifier.height(50.dp))
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(
-                text = "¡Bienvenido!",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(
-                text = "Inicia sesión en tu cuenta",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(40.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.icon),
-                contentDescription = "Icono de App",
-                modifier = Modifier
-                    .height(175.dp)
-                    .width(175.dp)
-            )
-        }
+        WelcomeText("Inicia sesión en tu cuenta")
 
         UsernameTextField(
             text = username,
@@ -99,7 +67,9 @@ fun AuthScreen(authVM: AuthViewModel = AuthViewModel()) {
 
         ) {
             Column(
-                modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 LabelClickable(
