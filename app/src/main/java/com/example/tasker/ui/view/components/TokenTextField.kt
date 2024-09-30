382876a18ecabfaed35c2bd7ab47afc4c85949b9
marvnramos <mframos@innovacion.gob.sv>
2024-09-30 09:00:31 -0600

    feat(ui): add token text field component

--- app/src/main/java/com/example/tasker/ui/view/components/TokenTextField.kt
+++ app/src/main/java/com/example/tasker/ui/view/components/TokenTextField.kt
@@ -10,32 +10,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tasker.ui.view.theme.GreyTitle
-import com.example.tasker.ui.view.theme.GreyTitleBorder
-import com.example.tasker.ui.view.theme.Orange
-import com.example.tasker.ui.view_model.AuthViewModel
-
-@Preview
-@Composable
-fun TokenTextFieldPreview(authVM: AuthViewModel = AuthViewModel()) {
-    val token by authVM.token.observeAsState(initial = "")
-
-//    TokenTextField(
-//        text = token,
-//        onTextChanged = {
-//            authVM.onValueChanged(token = it)
-//        }
-//    )
-}
 
 @Composable
 fun TokenTextField(
@@ -49,13 +28,11 @@ fun TokenTextField(
         imeAction = ImeAction.Done
     )
 
-//    val isValid = text.length == 6
-//    val errorIndicator = text.isNotEmpty() && !isValid
-
     val colorScheme = MaterialTheme.colorScheme
-    val indicatorColor = if (isValid) colorScheme.primary else colorScheme.error
-    val unfocusedIndicatorColor = if (isValid) colorScheme.outline else colorScheme.error
-    val labelColor = if (isValid) colorScheme.primary else colorScheme.error
+    val indicatorColor = if (isValid && !isError) colorScheme.primary else colorScheme.error
+    val unfocusedIndicatorColor =
+        if (isValid && !isError) colorScheme.outline else colorScheme.error
+    val labelColor = if (isValid && !isError) colorScheme.primary else colorScheme.error
     val placeholderColor = colorScheme.onSurface.copy(alpha = 0.6f)
     val containerColor = colorScheme.surface
 
@@ -87,7 +64,7 @@ fun TokenTextField(
                 .padding(10.dp),
             textStyle = MaterialTheme.typography.bodyMedium,
             placeholder = { Text(text = "000000", style = MaterialTheme.typography.bodyMedium) },
-            isError = isError,
+            isError = !isValid && isError,
             singleLine = true,
             maxLines = 1,
             keyboardOptions = keyboardOptions,
