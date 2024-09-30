27a11c3389fef0f87362aef092f36a3f5071ccf2
marvnramos <mframos@innovacion.gob.sv>
2024-09-30 14:14:20 -0600

    feat(ui): add 10dp space

--- app/src/main/java/com/example/tasker/ui/view/screens/SingUpScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/SingUpScreen.kt
@@ -1,14 +1,18 @@
 package com.example.tasker.ui.view.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.MutableLiveData
 import com.example.tasker.ui.view.components.auth.PasswordTextField
 import com.example.tasker.ui.view.components.auth.UsernameTextField
@@ -65,6 +69,7 @@ fun SingUpScreen(viewModel: AuthViewModel = AuthViewModel()) {
             text = username,
             onValueChange = { viewModel.onValueChanged(username = it) }
         )
+        Spacer(modifier = Modifier.height(10.dp))
         PasswordTextField(
             text = password,
             availableValidation = true,
