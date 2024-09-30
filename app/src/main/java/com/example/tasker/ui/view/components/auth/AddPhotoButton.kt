8b87ad47c5dc06d044f8e27a3da83ad77dcd1cd0
marvnramos <mframos@innovacion.gob.sv>
2024-09-30 14:29:39 -0600

    feat(ui): add photo button component

---
+++ app/src/main/java/com/example/tasker/ui/view/components/auth/AddPhotoButton.kt
@@ -0,0 +1,44 @@
+package com.example.tasker.ui.view.components.auth
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddPhotoButton() {
+    Card(
+        onClick = {},
+        modifier = Modifier.size(175.dp),
+        elevation = CardDefaults.elevatedCardElevation(2.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        shape = CircleShape
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.AddAPhoto,
+                contentDescription = "Add photo button",
+                tint = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+    }
+}
