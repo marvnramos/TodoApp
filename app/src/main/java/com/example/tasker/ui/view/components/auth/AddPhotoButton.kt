01ccc3028fa3f95b5917de24926bed54963d20c1
marvnramos <mframos@innovacion.gob.sv>
2024-10-01 11:41:08 -0600

    feat(ui): add image

--- app/src/main/java/com/example/tasker/ui/view/components/auth/AddPhotoButton.kt
+++ app/src/main/java/com/example/tasker/ui/view/components/auth/AddPhotoButton.kt
@@ -1,7 +1,15 @@
 package com.example.tasker.ui.view.components.auth
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -14,7 +22,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.tasker.ui.view.theme.TaskerTheme
+import coil.compose.rememberImagePainter
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.scale
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,12 +52,36 @@ fun AddPhotoButton(onClick: () -> Unit) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
+//            Image(
+//                painter = rememberAsyncImagePainter("https://www.pipsticks.com/cdn/shop/files/20240422094615_E9664F97.png?v=1713822329"),
+//                contentDescription = "",
+//                contentScale = ContentScale.Crop,
+//            )
             Icon(
                 imageVector = Icons.Default.AddAPhoto,
                 contentDescription = "Add photo button",
                 tint = MaterialTheme.colorScheme.onSurface,
                 modifier = Modifier.size(50.dp)
             )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddPhotoButtonPreview() {
+
+    TaskerTheme {
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            AddPhotoButton {
+                println("clickeeeeeeeeeeeeeeddddddddd! >:p")
+            }
+
         }
     }
 }
