d8333e2ce187a07de704082abc2e45c210c01152
marvnramos <mframos@innovacion.gob.sv>
2024-10-15 10:53:00 -0600

    feat(view): add navigation feature

--- app/src/main/java/com/example/tasker/ui/view/screens/EmailScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/EmailScreen.kt
@@ -3,14 +3,16 @@ package com.example.tasker.ui.view.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.NavHostController
 import com.example.tasker.ui.view.components.auth.EmailTextField
-import com.example.tasker.ui.view.theme.TaskerTheme
 import com.example.tasker.ui.view_model.AuthViewModel
 
 @Composable
-fun EmailScreen(authVM: AuthViewModel = AuthViewModel()) {
+fun EmailScreen(
+    navController: NavHostController,
+    authVM: AuthViewModel = AuthViewModel()
+) {
     val email by authVM.email.observeAsState(initial = "")
     val isValidEmail =
         if (email.isNotEmpty()) authVM.isEmailValid(MutableLiveData(email)) else true
@@ -19,8 +21,8 @@ fun EmailScreen(authVM: AuthViewModel = AuthViewModel()) {
     val isError = errorMessage != null
     val isAvailable = email.isNotEmpty() && !isError
     SingUpTemplate(
-        onArrowClick = { /*TODO*/ },
-        onTextClick = { /*TODO*/ },
+        onArrowClick = { navController.popBackStack() },
+        onTextClick = { navController.popBackStack() },
         onSubmitClick = { println("código enviado jiji") },
         subWelcomeText = "Ingresa tu correo electrónico",
         textIndicator = null,
@@ -37,10 +39,10 @@ fun EmailScreen(authVM: AuthViewModel = AuthViewModel()) {
         })
 }
 
-@Preview
-@Composable
-fun EmailScreenPreview() {
-    TaskerTheme {
-        EmailScreen()
-    }
-}
+//@Preview
+//@Composable
+//fun EmailScreenPreview() {
+//    TaskerTheme {
+//        EmailScreen()
+//    }
+//}
