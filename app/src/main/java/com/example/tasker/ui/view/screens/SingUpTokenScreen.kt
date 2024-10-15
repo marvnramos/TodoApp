9cbdb2b04f6e2933470ae600ce8546c55d20989e
marvnramos <mframos@innovacion.gob.sv>
2024-10-15 11:36:30 -0600

    feat(view): add navigation controller

--- app/src/main/java/com/example/tasker/ui/view/screens/SingUpTokenScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/SingUpTokenScreen.kt
@@ -6,28 +6,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.tasker.ui.view.components.TokenTextField
 import com.example.tasker.ui.view.theme.TaskerTheme
 import com.example.tasker.ui.view_model.AuthViewModel
 
 @Composable
-fun SingUpTokenScreen(authVM: AuthViewModel = AuthViewModel()) {
+fun SingUpTokenScreen(
+    navController: NavHostController,
+    authVM: AuthViewModel = AuthViewModel()
+) {
     val token by authVM.token.observeAsState(initial = "")
+
     val isValidToken = authVM.isTokenValid(token)
+    val isAvailable = token.isNotEmpty() && isValidToken
 
     val context = LocalContext.current
     SingUpTemplate(
-        onArrowClick = { Toast.makeText(context, "Arrow Clicked", Toast.LENGTH_SHORT).show() },
-        onTextClick = { Toast.makeText(context, "Text Clicked", Toast.LENGTH_SHORT).show() },
+        onArrowClick = { navController.popBackStack() },
+        onTextClick = { navController.navigate("Login") },
         onSubmitClick = { Toast.makeText(context, "Submit Clicked", Toast.LENGTH_SHORT).show() },
         subWelcomeText = "Verificación de correo",
         textIndicator = "Ingresa el código que recibiste \nen tu correo electrónico",
         submitText = "Verificar",
-        isAvailable = isValidToken,
+        isAvailable = isAvailable,
         textFieldComponent = {
             TokenTextField(
                 text = token,
-                isValid = isValidToken,
+                isValid = if (token.isNotEmpty()) isValidToken else true,
                 isError = token.isNotEmpty() && !isValidToken
             ) {
                 authVM.onValueChanged(token = it)
@@ -40,6 +47,7 @@ fun SingUpTokenScreen(authVM: AuthViewModel = AuthViewModel()) {
 @Composable
 fun SingUpTokenScreenPreview() {
     TaskerTheme {
-        SingUpTokenScreen()
+        val navController = rememberNavController()
+        SingUpTokenScreen(navController)
     }
 }
