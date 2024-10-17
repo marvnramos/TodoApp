96f1af5c8f190cbefd6ca8a43ebd02e78dbc180e
marvnramos <mframos@innovacion.gob.sv>
2024-10-17 10:20:32 -0600

    feat(view): add push to token screen function

--- app/src/main/java/com/example/tasker/ui/view/screens/EmailScreen.kt
+++ app/src/main/java/com/example/tasker/ui/view/screens/EmailScreen.kt
@@ -3,9 +3,13 @@ package com.example.tasker.ui.view.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.tasker.ui.view.Routes
 import com.example.tasker.ui.view.components.auth.EmailTextField
+import com.example.tasker.ui.view.theme.TaskerTheme
 import com.example.tasker.ui.view_model.AuthViewModel
 
 @Composable
@@ -20,10 +24,10 @@ fun EmailScreen(
     val errorMessage = if (!isValidEmail) "Dirección de correo inválida" else null
     val isError = errorMessage != null
     val isAvailable = email.isNotEmpty() && !isError
-    SingUpTemplate(
+    Template(
         onArrowClick = { navController.popBackStack() },
         onTextClick = { navController.popBackStack() },
-        onSubmitClick = { println("código enviado jiji") },
+        onSubmitClick = { pushToTokenScreen(navController) },
         subWelcomeText = "Ingresa tu correo electrónico",
         textIndicator = null,
         submitText = "Enviar código",
@@ -36,13 +40,19 @@ fun EmailScreen(
                 errorMessage = errorMessage,
                 onTextChanged = { authVM.onValueChanged(email = it) }
             )
-        })
+        },
+        loginAvailable = false
+    )
 }
 
-//@Preview
-//@Composable
-//fun EmailScreenPreview() {
-//    TaskerTheme {
-//        EmailScreen()
-//    }
-//}
+@Preview
+@Composable
+fun EmailScreenPreview() {
+    TaskerTheme {
+        EmailScreen(navController = rememberNavController())
+    }
+}
+
+private fun pushToTokenScreen(navController: NavHostController) {
+    navController.navigate(Routes.TOKEN_FORGOT_PASSWORD_FLOW)
+}
