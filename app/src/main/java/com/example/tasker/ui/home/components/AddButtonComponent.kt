be3e66e4f229bf5d39d1a65ca36ebd93ee1847e8
marvnramos <mframos@innovacion.gob.sv>
2024-10-18 13:27:49 -0600

    feat(view): add dynamic color base on theme

--- app/src/main/java/com/example/tasker/ui/home/components/AddButtonComponent.kt
+++ app/src/main/java/com/example/tasker/ui/home/components/AddButtonComponent.kt
@@ -5,21 +5,20 @@ import androidx.compose.material3.Icon
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material3.MaterialTheme
 import androidx.core.content.ContextCompat
 import com.example.tasker.R
 
 
-@Preview
 @Composable
-fun AddButtonComponent(){
+fun AddButtonComponent(onClick: () -> Unit) {
     val context = LocalContext.current
     FloatingActionButton(
-        onClick = { /*TODO: Open 2nd screen*/ },
-        containerColor = Color(ContextCompat.getColor(context, R.color.orange)),
-        contentColor = Color(ContextCompat.getColor(context, R.color.white)),
+        onClick = { onClick() },
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
 
     ) {
         Icon(Icons.Filled.Add, contentDescription = "Floating action button.")
