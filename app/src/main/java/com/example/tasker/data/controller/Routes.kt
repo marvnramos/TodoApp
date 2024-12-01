b2b7cce65b8d4aefe2835e8dcade2bf36d86e7a7
marvnramos <mr487223@gmail.com>
2024-12-01 12:33:29 -0600

    feat: add Routes file

---
+++ app/src/main/java/com/example/tasker/data/controller/Routes.kt
@@ -0,0 +1,14 @@
+package com.example.tasker.data.controller
+
+object Routes {
+    const val LOGIN = "Login"
+    const val EMAIL_FORGOT_PASSWORD_FLOW = "EmailForgotPasswordFlow"
+    const val EMAIL_SIGN_UP_FLOW = "EmailSignUpFlow"
+    const val TOKEN_SIGN_UP_FLOW = "TokenSignUpFlow"
+    const val SIGN_UP = "SingUp"
+    const val TOKEN_FORGOT_PASSWORD_FLOW = "TokenForgotPasswordFlow"
+    const val FORGOT_PASSWORD = "ForgotPassword"
+    const val HOME = "Home"
+    const val ARCHIVED = "Archived"
+    const val FRIENDS = "Friends"
+}
